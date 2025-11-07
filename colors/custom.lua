a55f856844dcd070c4771092654a37dbb7ff03de
yylt <yang8518296@163.com>
2025-11-07 12:30:06 +0800

    update default shell, font, bindings, colorscheme, new solarized_light colorscheme

--- colors/custom.lua
+++ colors/custom.lua
@@ -29,6 +29,89 @@ local mocha = {
    crust     = '#11111b',
 }
 
+local solarized = {
+  base03  = '#001a21',  -- 接近黑色的深蓝绿
+  base02  = '#04262e',  -- 深背景元素
+  base01  = '#3a585f',  -- 中等对比文本
+  base00  = '#4a656d',  -- 主体文本
+  base0   = '#5a7880',  -- 次要文本
+  base1   = '#6a868e',  -- 注释
+  base2   = '#e0d9c3',  -- 浅背景
+  base3   = '#f5ecd5',  -- 最浅背景
+  
+  yellow  = '#d4a000',
+  orange  = '#e55b20',
+  red     = '#f03025',
+  magenta = '#eb2080',
+  violet  = '#7055d4',
+  blue    = '#0087ff',
+  cyan    = '#00a89f',
+  green   = '#85a600'
+}
+
+-- Solarized Light 配色方案
+local solarized_light = {
+  foreground = solarized.base00,
+  background = solarized.base3,
+  cursor_bg = solarized.base01,
+  cursor_border = solarized.base01,
+  cursor_fg = solarized.base3,
+  selection_bg = solarized.base2,
+  selection_fg = solarized.base00,
+  ansi = {
+    solarized.base2,   -- black
+    solarized.red,     -- red
+    solarized.green,   -- green
+    solarized.yellow,  -- yellow
+    solarized.blue,    -- blue
+    solarized.magenta, -- magenta/purple
+    solarized.cyan,    -- cyan
+    solarized.base02   -- white
+  },
+  brights = {
+    solarized.base3,   -- black
+    solarized.orange,  -- red
+    solarized.base1,   -- green
+    solarized.base0,   -- yellow
+    solarized.base00,  -- blue
+    solarized.violet,  -- magenta/purple
+    solarized.base01,  -- cyan
+    solarized.base03   -- white
+  },
+  tab_bar = {
+    background = solarized.base2,
+    active_tab = {
+      bg_color = solarized.base1,
+      fg_color = solarized.base03,
+    },
+    inactive_tab = {
+      bg_color = solarized.base2,
+      fg_color = solarized.base01,
+    },
+    inactive_tab_hover = {
+      bg_color = solarized.base2,
+      fg_color = solarized.base00,
+    },
+    new_tab = {
+      bg_color = solarized.base3,
+      fg_color = solarized.base00,
+    },
+    new_tab_hover = {
+      bg_color = solarized.base2,
+      fg_color = solarized.base00,
+      italic = true,
+    },
+  },
+  visual_bell = solarized.red,
+  indexed = {
+    [16] = solarized.orange,
+    [17] = solarized.red,
+  },
+  scrollbar_thumb = solarized.base1,
+  split = solarized.base0,
+  compose_cursor = solarized.yellow,
+}
+
 local colorscheme = {
    foreground = mocha.text,
    background = mocha.base,
@@ -39,22 +122,22 @@ local colorscheme = {
    selection_fg = mocha.text,
    ansi = {
       '#0C0C0C', -- black
-      '#C50F1F', -- red
+      '#f03025', -- red
       '#13A10E', -- green
-      '#C19C00', -- yellow
-      '#0037DA', -- blue
-      '#881798', -- magenta/purple
-      '#3A96DD', -- cyan
+      '#FFDF20', -- yellow
+      '#0087ff', -- blue
+      '#FF692A', -- magenta/purple
+      '#46ECD5', -- cyan
       '#CCCCCC', -- white
    },
    brights = {
       '#767676', -- black
       '#E74856', -- red
       '#16C60C', -- green
-      '#F9F1A5', -- yellow
-      '#3B78FF', -- blue
-      '#B4009E', -- magenta/purple
-      '#61D6D6', -- cyan
+      '#FFDF20', -- yellow
+      '#51A2FF', -- blue
+      '#FF692A', -- magenta/purple
+      '#46ECD5', -- cyan
       '#F2F2F2', -- white
    },
    tab_bar = {
